15ca0f03f2f46ef7282421598f69163cde9a05ad
Michael Weiss <dev.primeos@gmail.com>
2016-11-02 22:49:20 +0100

    mattermost: Build from source

--- pkgs/servers/mattermost/default.nix
+++ pkgs/servers/mattermost/default.nix
@@ -1,23 +1,25 @@
-{ stdenv, fetchurl }:
+{ stdenv, fetchFromGitHub }:
 
 stdenv.mkDerivation rec {
   name = "mattermost-${version}";
   version = "3.6.2";
 
-  src = fetchurl {
-    url = "https://releases.mattermost.com/${version}/mattermost-team-${version}-linux-amd64.tar.gz";
-    sha256 = "1wf8xvy8njmhym45dwag6mdwhfgzg5xccvhyv7c68rz5h57vfgsb";
+  src = fetchFromGitHub {
+    owner = "mattermost";
+    repo = "platform";
+    rev = "v${version}";
+    sha256 = "1c7msfl49mfik1c0bjd51df01hylxk8sx29206f00hnd8nax71i0";
   };
 
-  installPhase = ''
-    mkdir -p $out
-    mv * $out/
-    ln -s ./platform $out/bin/mattermost-platform
-  '';
-
-  postFixup = ''
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/platform
-  '';
+#  installPhase = ''
+#    mkdir -p $out
+#    mv * $out/
+#    ln -s ./platform $out/bin/mattermost-platform
+#  '';
+#
+#  postFixup = ''
+#    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/platform
+#  '';
 
   meta = with stdenv.lib; {
     description = "Open-Source, self-hosted Slack-alternative";
