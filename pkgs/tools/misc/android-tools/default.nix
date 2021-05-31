b12e2e6db2deeead032728ce8dd2a0b7b8847900
Michael Weiss <dev.primeos@gmail.com>
2021-05-31 22:00:59 +0200

    android-tools: Fetch the Go modules

--- pkgs/tools/misc/android-tools/default.nix
+++ pkgs/tools/misc/android-tools/default.nix
@@ -1,19 +1,33 @@
-{ lib, stdenv, fetchurl
+{ lib, stdenv, fetchurl, buildGoModule
 , cmake, perl, go
 , protobuf, zlib, gtest, brotli, lz4, zstd, libusb1, pcre2
 }:
 
-stdenv.mkDerivation rec {
+let
   pname = "android-tools";
   version = "31.0.0p1";
-
   src = fetchurl {
     url = "https://github.com/nmeum/android-tools/releases/download/${version}/android-tools-${version}.tar.xz";
     sha256 = "1dn7v10gdx1pi0pkddznd5sdz941qz0x4jww8h2mk50nbyxc792i";
   };
+  boringsslGoModules = (buildGoModule {
+    inherit pname version;
+    inherit src;
+    modRoot = "vendor/boringssl";
+    vendorSha256 = "0w690j49mhd1ssrf8ls47s5q5y5v1pnjmnvcyyxk5nfjadcmbq97";
+  }).go-modules;
+in stdenv.mkDerivation rec {
+  inherit pname version;
+
+  inherit src;
+  postPatch = ''
+    cp -r --reflink=auto ${boringsslGoModules} vendor/boringssl/vendor
+  '';
 
   nativeBuildInputs = [ cmake perl go ];
   buildInputs = [ protobuf zlib gtest brotli lz4 zstd libusb1 pcre2 ];
+
+  GOFLAGS = [ "-mod=vendor" ];
 
   meta = with lib; {
     description = "Android SDK platform tools";
